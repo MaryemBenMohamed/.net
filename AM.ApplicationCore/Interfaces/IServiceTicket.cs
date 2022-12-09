3d6c0e1d2d250e96a444f9568ae55fe9aa9b3071
GestionProduits <maryem.benmohamed@esprit.tn>
2022-12-09 20:30:24 +0100

    Initial commit

---
+++ AM.ApplicationCore/Interfaces/IServiceTicket.cs
@@ -0,0 +1,16 @@
+﻿using AM.ApplicationCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Interfaces
+{
+   public interface IServiceTicket: IService<Ticket>
+    {
+        public IEnumerable<Ticket> GetTicketByPrice(string prix);
+        public IEnumerable<Ticket> GetTicketBySiege(string siege);
+        public IEnumerable<Ticket> GetTicketByPriceAndSiege(string prix, string siege);
+    }
+}
---
+++ AM.ApplicationCore/Services/ServiceTicket.cs
@@ -0,0 +1,35 @@
+﻿using AM.ApplicationCore.Services;
+using AM.ApplicationCore.Interfaces;
+using AM.ApplicationCore.Domain;
+using System.Text.RegularExpressions;
+
+namespace AM.ApplicationCore.Interfaces
+{
+    public class ServiceTicket : Service<Ticket>, IServiceTicket
+    {
+        public ServiceTicket(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public IEnumerable<Ticket> GetTicketByPrice(string prix)
+        {
+            return GetMany(m => m.Prix >= float.Parse(prix));
+            //return GetMany(m => m.Prix >= float.Parse(prix)).ToList();
+
+        }
+
+        public IEnumerable<Ticket> GetTicketBySiege(string siege)
+        {
+            return GetMany(m => m.Siege == int.Parse(siege));
+
+        }
+
+        public IEnumerable<Ticket> GetTicketByPriceAndSiege(string prix,string siege)
+        {
+            return GetMany(m =>m.Prix >= float.Parse(prix) && m.Siege >= float.Parse(siege));  
+
+        }
+
+
+    }
+}
